bebb7991ad02368fe265d777ef10b5f134854578
Yura Loginov <yuretsmolodets@yandex.ru>
2026-09-01 19:51:37 +0300

    chore: move database into data/ directory

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 import Database from 'better-sqlite3';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dbPath = path.join(__dirname, '..', 'news.db');
+const dataDir = path.join(__dirname, '..', 'data');
+
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, 'news.db');
 
 const db = new Database(dbPath);
 
